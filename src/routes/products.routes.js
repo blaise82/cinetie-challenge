3eb6854b25b54694dddb2d69272db83b0438bfdc
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-06-13 14:25:48 +0200

    ft(products): delete product

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import productController from '../controllers/productController';
 import productValidation from '../validations/product.validate';
 import checkLogin from '../middlewares/checkLogin';
+import validateId from '../validations/id.validate';
 
 const router = express.Router();
 
 router.post('/add', checkLogin, productValidation, productController.add);
 router.get('/get-all', checkLogin, productController.getAll);
+router.delete('/delete', checkLogin, validateId, productController.delete);
 
 export default router;
---
+++ src/validations/id.validate.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+const validateId = (req, res, next) => {
+  const Schemas = Joi.object().keys({
+    accommodationId: Joi.string().guid().error(() => ({
+      message: 'An id is not provided'
+    })),
+  });
+  const { error } = Joi.validate(req.query, Schemas);
+  if (error) {
+    return res.status(400).json({
+      status: 400,
+      error: error.details[0].message
+    });
+  }
+  next();
+};
+export default validateId;
